5c6d1de46c9ad81e24c05d8ecb2667c98b584507
Hamidov Akbar <akbar.hamidov.02@gmail.com>
2023-01-30 23:36:06 +0100

    Initial commit

---
+++ src/main/java/com/global/clover/clover/CloverApplication.java
@@ -0,0 +1,13 @@
+package com.global.clover.clover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/global/clover/clover/CloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.clover.clover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
